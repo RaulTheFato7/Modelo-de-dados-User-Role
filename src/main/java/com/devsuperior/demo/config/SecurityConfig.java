10b7f14be6012e855c9a7a632009a9db9f21374a
Raul Vitorio <raulvitorio790@gmail.com>
2026-03-19 10:54:16 -0300

    Implementando o Spring Security

---
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.devsuperior.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
