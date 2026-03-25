96201a25464fb3cd463a74555791e9d6d4d30236
Raul Vitorio <raulvitorio790@gmail.com>
2026-03-25 08:54:51 -0300

    Authorization server, Resource sercer, ainda nao testado.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$5h3l9OwgD0cDAGmpT/.Jie6Wkrp.k0ETqYY1ZpMiYRFeshCKWq122');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$5h3l9OwgD0cDAGmpT/.Jie6Wkrp.k0ETqYY1ZpMiYRFeshCKWq122');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
